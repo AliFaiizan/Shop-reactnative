2b0c237c56327b3cb4687d72646a25e71a56d4c1
AliFaiizan <alifaizanahmad@gmail.com>
2022-04-10 18:03:00 +0500

    fetchig product

--- screens/shop/ProductsOverview.tsx
+++ screens/shop/ProductsOverview.tsx
@@ -1,9 +1,10 @@
 import { View, FlatList } from "react-native";
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
 import ProductListItem from "../../components/shop/ProductListItem";
 import { Color } from "../../constants/Colors";
 import * as CartActions from "../../store/actions/cart-actions";
+import * as ProductActions from "../../store/actions/product-action";
 
 import CHeaderButton from "../../components/UI/CHeaherButton";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
@@ -16,6 +17,11 @@ const ProductsOverview: FC = (props: any) => {
   const products = useSelector(
     (state: RootStateOrAny) => state.products.availableProducts
   );
+
+  useEffect(() => { 
+    dispatch(ProductActions.fetchProducts)
+   },[dispatch])
+   
   return (
     <View style={{ backgroundColor: Color.skin }}>
       <FlatList
--- store/actions/product-action.ts
+++ store/actions/product-action.ts
@@ -2,11 +2,38 @@
 export const DELETE_PRODUCT='DELETE_PRODUCT';
 export const CREATE_PRODUCT='CREATE_PRODUCT';
 export const UPDATE_PRODUCT='UPDATE_PRODUCT';
+export const SET_PRODUCTS='SET_PRODUCTS';
 
 //this will delete item from store
 export const deleteProduct=(prodId:string) => { 
     return {type:DELETE_PRODUCT,prodId}
 }
+
+export const fetchProducts=() => { 
+    return async (dispatch:Function )=>{
+
+    //any async code you want
+    const responce = await fetch('https://onlineshop-e7753-default-rtdb.firebaseio.com/products.json')
+
+    const resData= await responce.json();
+
+    const loadedProducts=[];
+    for( const key in resData){
+
+        const {title,description,imageUrl,price}=resData[key]
+        loadedProducts.push({
+          key,
+          ownerId:'u1',
+          title,
+          description,
+          imageUrl,
+          price,
+        });
+    }
+
+    dispatch({type:SET_PRODUCTS,products:loadedProducts})
+
+ }}
 
 //action for creating item in store
 export const createProduct=(title:string,description:string,imageUrl:string,price:number)=>{
