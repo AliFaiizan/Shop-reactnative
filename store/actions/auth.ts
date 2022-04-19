ac662a1306048f125550ca67807fd368b8c2b0e5
AliFaiizan <alifaizanahmad@gmail.com>
2022-04-19 23:58:50 +0500

    login actions

--- store/actions/auth.ts
+++ store/actions/auth.ts
@@ -1,3 +1,6 @@
+import { GOOGLE_API } from "../../secrets";
+
+
 export const SIGN_UP='SING_UP';
 
 
@@ -5,7 +8,7 @@ export const signUp=(email:any,password:any) => {
     return async (dispatch:any)=>{
 
         const response = await fetch(
-          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCBVPoG6pdMe6PTWWO1zrm-6RwaNb9mosE",
+          `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${GOOGLE_API}`,
           {
             method: "POST",
             headers: {
@@ -26,3 +29,28 @@ export const signUp=(email:any,password:any) => {
         dispatch({type:SIGN_UP})
     }
  }
+
+ export const login = (email: any, password: any) => {
+   return async (dispatch: any) => {
+     const response = await fetch(
+       `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${GOOGLE_API}`,
+       {
+         method: "POST",
+         headers: {
+           "Content-Type": "application/json",
+         },
+         body: JSON.stringify({ email, password, returnSecureToken: true }),
+       }
+     );
+
+     if (!response.ok) {
+       throw new Error("Something Went wrong");
+     }
+
+     const resData = await response.json();
+
+     console.log(resData);
+
+     dispatch({ type: SIGN_UP });
+   };
+ };
